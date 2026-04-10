08611b4bc2758b0f700a429d52a98995961f1563
ButtySaylee <nbs917740@gmail.com>
2026-04-10 12:50:07 +0530

    added new category

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -8,7 +8,7 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export type Product = {
   id: string;
   name: string;
-  category: 'earrings' | 'footwear' | 'bags';
+  category: 'earrings' | 'footwear' | 'bags' | 'other';
   price: number;
   description: string;
   images: string[];
